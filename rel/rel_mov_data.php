fba02dad70df823f72e164d46d65d77294046213
Robson Miranda <robson1992ferreira@hotmail.com>
2020-08-25 11:49:39 -0300

    relatorio pdf movimentacoes

--- rel/rel_mov_data.php
+++ rel/rel_mov_data.php
@@ -4,10 +4,18 @@ $dataInicial = $_GET['dataInicial'];
 $dataFinal = $_GET['dataFinal'];
 $tipo = $_GET['tipo'];
 
+if($tipo != 'Todas' and $tipo != 'Entrada'){
+	$tipo = 'Saída';
+}
+
+
 $dataIni = implode('/', array_reverse(explode('-', $dataInicial)));
 $dataFin = implode('/', array_reverse(explode('-', $dataFinal)));
 
 include('../conexao.php');
+
+
+
  ?>
 
 
@@ -83,10 +91,16 @@ include('../conexao.php');
 		
 			<div class="row">
 				<div class="col-sm-6">	
-				   <big><big> RELATÓRIO DE MOVIMENTAÇÕES   </big> </big> 
+				   <big><big> RELATÓRIO DE MOVIMENTAÇÕES  </big> </big> 
 				</div>
 				<div class="col-sm-6">	
-				<small></small>
+				<small><?php 
+					if($tipo == 'Todas'){
+						echo 'Todas as Movimentações';
+					}else{
+						echo 'Movimentações de '.$tipo;
+					}
+				 ?></small>
 				</div>
 				
 			</div>
@@ -109,76 +123,104 @@ include('../conexao.php');
 		<br><br>
 
 		<?php
-         $total_gastos= 0;
-         $quant = 0;
+
+		$total_mov = 0;
+		$quant = 0;
+		$quant_entradas = 0;
+		$quant_saidas = 0;
+		$total_entradas = 0;
+		$total_saidas = 0;
+
+		 if($tipo != 'Todas'){
                           
-            $query ="select * from gastos where data >= '$dataInicial' and data <= '$dataFinal' order by data asc";
+                           $query = "select * from movimentacoes where (data >= '$dataInicial' and data <= '$dataFinal') and tipo = '$tipo' order by data asc";
+                       }
+                       else{
+                       	$query = "select * from movimentacoes where data >= '$dataInicial' and data <= '$dataFinal' order by data asc";
+                       }
 
-                
+                           
 
-            $result = mysqli_query($conexao, $query);
-            //$dado = mysqli_fetch_array($result);
-            $row = mysqli_num_rows($result);
+                        $result = mysqli_query($conexao, $query);
+                        //$dado = mysqli_fetch_array($result);
+                        $row = mysqli_num_rows($result);
 
-            if($row == ''){
+                        if($row == ''){
 
-                echo "<h3> Não existem dados cadastrados no banco </h3>";
+                            echo "<h3> Não existem dados cadastrados no banco </h3>";
 
-            }else{
+                        }else{
 
-             ?>
+                           ?>
 
 
 		<table class="table">
 			<tr bgcolor="#f9f9f9">
-				<td style="font-size:12px"> <b>Motivo</b> </td>
-				<td style="font-size:12px"> <b>Valor</b> </td>
+				<td style="font-size:12px"> <b>Tipo</b> </td>
+				<td style="font-size:12px"> <b>Movimento</b> </td>
+				<td style="font-size:12px"> <b> Valor</b> </td>
 				<td style="font-size:12px"> <b> Funcionário</b> </td>
+				
 				<td style="font-size:12px"> <b> Data</b> </td>
-				<td style="font-size:12px"> <b> Status</b> </td>
 				
 			</tr>
 			
 
-                 <?php 
-                          
+				 <?php 
+				  			
+				  			
 
                           while($res_1 = mysqli_fetch_array($result)){
-                            $motivo = $res_1["motivo"];
+                            $tipo_mov = $res_1["tipo"];
+                            $movimento = $res_1["movimento"];
                             $valor = $res_1["valor"];
                             $funcionario = $res_1["funcionario"];
+                           
                             $data = $res_1["data"];
-                           
 
-                           
-    
 
-                            $data_2 = implode('/', array_reverse(explode('-', $data)));
+                            
+
+
+                            $data2 = implode('/', array_reverse(explode('-', $data)));
 							
 							
+
                            
-                            $total_gastos = $total_gastos + $valor;
+                            $total_mov = $total_mov + $valor;
                             $quant = $quant + 1;
 
-                
+                            if($tipo_mov == 'Entrada'){
+                            	$quant_entradas = $quant_entradas + 1;
+                            	$total_entradas = $total_entradas + $valor;
+                            }
+
+                            if($tipo_mov == 'Saída'){
+                            	$quant_saidas = $quant_saidas + 1;
+                            	$total_saidas = $total_saidas + $valor;
+                            }
                          
+                         	
+                         
+                         	
                          
                          
                             ?>
 
                 <tr>
-                    <td style="font-size:12px"> <?php echo $motivo; ?> </td>
-                    <td style="font-size:12px"> R$<?php echo $valor; ?> </td>
-                    <td style="font-size:12px"> <?php echo $funcionario; ?> </td>
-                    <td style="font-size:12px"> <?php echo $data_2; ?> </td>
-                    
+				<td style="font-size:12px"> <?php echo $tipo_mov; ?> </td>
+				<td style="font-size:12px"> <?php echo $movimento; ?> </td>
+				<td style="font-size:12px"> R$ <?php echo $valor; ?> </td>
+				<td style="font-size:12px"> <?php echo $funcionario; ?> </td>
+				
+				<td style="font-size:12px"> <?php echo $data2; ?> </td>
 				
 				</tr>
 
-			<?php  } ?>
+			<?php }  ?>
 		</table>
 
-	<?php } ?>
+	<?php }  ?>
 
 
 		<hr>
@@ -186,18 +228,86 @@ include('../conexao.php');
 
 		<hr>
 
-	
+		<?php
+		if($tipo == 'Todas'){
+
+			?>
 
 			<div class="row">
-				<div class="col-sm-8">	
-				 <small><b> Quantidade de Gastos:</b> <?php echo $quant; ?> </small>
+				<div class="col-sm-12">	
+				 <p style="font-size:12px">
+				 	 <b>Quantidade de Entradas:</b>  <?php echo $quant_entradas; ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				 	  <b>Quantidade de Saídas:</b>  <?php echo $quant_saidas; ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				 	  
+				 	   
+				 	
+
+				  </p>
 				</div>
-				<div class="col-sm-4">	
-				 <small><b>  Total OS:</b> R$<?php echo $total_gastos; ?>,00 </small>
+			</div>
+
+			<div class="row">
+
+				<div class="col-sm-7">	
+				 <p style="font-size:12px">
+				 	 <b>Valor das Entradas:</b> <font color="green"> R$ <?php echo $total_entradas; ?>,00 </font> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				 	  <b>Valor das Saídas:</b><font color="red"> R$ <?php echo $total_saidas; ?>,00 </font> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				 	  
+				 	   
+				 	
+
+				  </p>
+				</div>
+				<div class="col-sm-3">	
+				 <p style="font-size:12px" align="right">
+				 	 <b>Saldo Total:</b>
+				 	 <?php 
+				 	 $saldo = $total_entradas - $total_saidas;
+				 	 if($saldo >= 0){
+				 	 	?>
+				 	 	<font color="green">R$ <?php echo $saldo ?>,00 </font>
+				 	 	<?php 
+				 	 }else{
+				 	 	?>
+				 	 	<font color="red">R$ <?php echo $saldo ?>,00 </font> 
+				 	 	<?php 
+				 	 }
+
+				 	 ?>
+				 	 
+				 	 
+				 	   
+				 	
+
+				  </p>
 				</div>
 				
 			</div>
 
+		<?php }else{
+
+			?>
+
+			<div class="row">
+				<div class="col-sm-8">	
+				 <small><b> Quantidade de Movimentações:</b> <?php echo $quant; ?> </small>
+				</div>
+				<div class="col-sm-4">	
+				 <small><b> Valor Total:</b> R$<?php echo $total_mov; ?>,00 </small>
+				</div>
+				
+			</div>
+
+			<?php
+		}
+
+			?>
+		
+
+			
+			
+
+	
 </div>
 
 
